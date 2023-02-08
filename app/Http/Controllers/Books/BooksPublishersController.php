5228680cd94f11cb4bdf051c5b282eadc5c175d1
revgato <locchoihay147@gmail.com>
2023-02-08 22:22:08 +0700

    get publisher by book

--- app/Http/Controllers/Books/BooksPublishersController.php
+++ app/Http/Controllers/Books/BooksPublishersController.php
@@ -19,4 +19,19 @@ class BooksPublishersController extends Controller
             return response()->json(['Can not find books by publisher', 400]);
         }
     }
+
+    public function get_publisher_by_book($id) {
+        // return publisher by book
+        $publisher = BooksPublishers::where('book_id', '=', $id)->get('publisher_id');
+        if (count($publisher) != 0) {
+            $publisher_name = Publishers::where('id', '=', $publisher[0]['publisher_id'])->get('name');
+            if ($publisher_name != null) {
+                return response()->json($publisher_name, 200);
+            } else {
+                return response()->json(['Can not find publisher by book', 400]);
+            }
+        } else {
+            return response()->json(['Can not find publisher by book', 400]);
+        }
+    }
 }
